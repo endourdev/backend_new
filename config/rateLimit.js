982cb062065a9ca0bcf605b3147b6f6571b6e6dd
Mohamed Ouaouda <contact@erisium-pvp.fr>
2026-02-11 14:19:56 +0100

    Update rateLimit.js

--- config/rateLimit.js
+++ config/rateLimit.js
@@ -1,7 +1,7 @@
 const rateLimit = require("express-rate-limit");
 
 const apiLimiter = rateLimit({
-  windowMs: 1 * 60 * 1000, // 1 minute
+  windowMs: 1 * 60 * 100, // 1 minute
   max: 10,
   message: "Too many requests, please try again later.",
 });
